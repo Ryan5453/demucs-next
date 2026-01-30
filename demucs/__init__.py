070bcf713a2f000d65f8df1a386e3bdb347588f0
Ryan Fahey <git@ryan.science>
2026-01-30 01:32:59 -0500

    deps

--- demucs/__init__.py
+++ demucs/__init__.py
@@ -4,6 +4,16 @@
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
 
+import warnings
+
+# Suppress PyTorch internal resize warnings from STFT/iSTFT
+# These are benign and originate from PyTorch issue #134323
+warnings.filterwarnings(
+    "ignore",
+    message="An output with one or more elements was resized since it had shape",
+    category=UserWarning,
+)
+
 __version__ = "1.0.0.dev0"
 
 from .api import (
